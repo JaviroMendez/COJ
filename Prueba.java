d11fc6eae0e3e2d6c5183d3f33b87ab892e31937
Javier Mendez <36248601+JaviroMendez@users.noreply.github.com>
2019-08-08 18:31:41 -0500

    Create Prueba.java

---
+++ Prueba.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class E1 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a;
+        a=sc.nextInt();
+        int b;
+                b=sc.nextInt();
+                
+                int res;
+                res=a+b;
+                System.out.println(res);
+    }
+}
