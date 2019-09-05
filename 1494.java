f8cb18aec95eff18ef39024d6f294f9669757543
Javier Alejandro Mendez Rodriguez <36248601+JaviroMendez@users.noreply.github.com>
2019-09-05 16:57:43 -0500

    Create 1494.java

---
+++ 1494.java
@@ -0,0 +1,10 @@
+import java.io.*;
+public class noleas{
+public static void main(String []args)throws IOException{
+BufferedReader br=new BufferedReader(new InputStreamReader(System.in),1024*8);
+PrintWriter pw=new PrintWriter(System.out,false);
+br.readLine();
+pw.println("I got my first solution!!!");
+pw.close();
+}
+}
