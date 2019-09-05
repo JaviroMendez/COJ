1830214f0ad9a347ec74285bce941d93c86ea34b
Javier Alejandro Mendez Rodriguez <36248601+JaviroMendez@users.noreply.github.com>
2019-09-05 17:25:36 -0500

    Create 2694.java

---
+++ 2694.java
@@ -0,0 +1,9 @@
+import java.io.PrintWriter;
+
+public class MoreDifficulttoSolve {
+    public static void main(String[] args) {
+    PrintWriter pw=new PrintWriter(System.out,false);
+        pw.println("This is my first solution in programming competitions");
+        pw.close();
+}
+}
